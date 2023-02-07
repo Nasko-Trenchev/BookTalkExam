8b44efbb4d08ad6e0025d64b894821588eb7c729
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-07 17:33:06 +0200

    Adding router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { urlencoded } = require('express');
 const express = require('express');
 
 const config = require('./config')
 const setupViewEngine = require('./config/viewEngine');
+const router = require('./router');
 
 const app = express();
 
@@ -10,6 +10,8 @@ setupViewEngine(app);
 
 app.use(express.static('src/public'));
 app.use(urlencoded({extended: true}));
+app.use(router);
+
 
 app.listen(config.PORT, () => {
     console.log(`Server is listening on port... ${config.PORT}`)

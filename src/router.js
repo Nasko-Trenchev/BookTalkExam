2757fccaa700303316545d28ecf2dfff510497ed
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-07 17:51:00 +0200

    Adding register and login views

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
+const authController = require('./controllers/authController')
 
 
 router.get('/', homeController.getHomePage);
 router.get('/404', homeController.getErrorPage);
+
+router.get('/login', authController.getLoginPage);
+router.get('/register', authController.getRegisterPage)
 
 module.exports = router;
